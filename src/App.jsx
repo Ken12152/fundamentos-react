3f1f9de0f0f226d8ce1747c4d7961e38bd704075
Felipe Kenzo <Kenzo12152@gmail.com>
2020-11-01 15:17:56 -0300

    Filho

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,10 @@ import ComParametro from './components/base/ComParametro'
 import Fragmento from './components/base/Fragmento'
 import Random from './components/base/Random'
 
+import Familia from './components/base/Familia'
+
 import Card from './components/layout/Card'
+
 
 export default function App(props) {
     return (
@@ -15,9 +18,14 @@ export default function App(props) {
             <h1>Fundamentos React</h1>
 
             <div className="Cards">
-                <Card titulo="#05 Card" border="white">
-                    <h2>Criar Cards</h2>
+                <Card titulo="#05 Filho" color="#00c8f8">
+                    <Familia sobrenome="Snake" />
                 </Card>
+
+                {/* <Card titulo="#05 Card" border="white">
+                    <h2>Criar Cards</h2>
+                </Card> */}
+
                 <Card titulo="#04 Random" color="#7fff7f" border="yellow">
                     <Random min={50} max={100} />
                 </Card>
